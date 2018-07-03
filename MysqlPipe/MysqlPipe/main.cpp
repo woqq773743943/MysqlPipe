61219c77cb9790441bc71634cd12df1490a6952b
DaLaoChen <DaLaoChen@DALAOCHEN-PC>
2018-07-03 15:07:36 +0800

    正确的使用管道

--- MysqlPipe/MysqlPipe/main.cpp
+++ MysqlPipe/MysqlPipe/main.cpp
@@ -8,7 +8,7 @@ void invoke(string exe);
 
 int main(int argc, char* argv[])
 {
-	string exe = "C:\\Users\\DALAOCHEN\\Desktop\\mysql-5.7.10\\bld\\client\\Debug\\mysql.exe -uroot -proot";
+	string exe = "hello.exe";
 	invoke(exe);
 	return 0;
 }
@@ -21,38 +21,41 @@ void invoke(string exe)
 	saPipe.lpSecurityDescriptor = NULL;
 	saPipe.bInheritHandle = TRUE;
 
-	HANDLE hReadPipe, hWritePipe;
-	BOOL bSuccess = CreatePipe(&hReadPipe,
-		&hWritePipe,
+	HANDLE hInReadPipe, hInWritePipe;
+	if (!CreatePipe(&hInReadPipe,
+		&hInWritePipe,
 		&saPipe,
-		0);
-	if (!bSuccess)
+		0))
 		return;
 
+	HANDLE hOutReadPipe, hOutWritePipe;
+	if (!CreatePipe(&hOutReadPipe,
+		&hOutWritePipe,
+		&saPipe,
+		0))
+		return;
 	PROCESS_INFORMATION pi;
 	STARTUPINFO si;
 	memset(&si, 0, sizeof(si));
-	si.hStdInput = hReadPipe;
-	si.hStdOutput = hWritePipe;
+	si.hStdInput = hInReadPipe;
+	si.hStdError = hOutWritePipe;
+	si.hStdOutput = hOutWritePipe;
 	si.dwFlags = STARTF_USESTDHANDLES;
 	si.cb = sizeof(si);
 
-	if (CreateProcess(NULL, (char*)exe.c_str(), NULL, NULL, TRUE, 0, NULL, NULL, &si, &pi))
+	if (CreateProcess(NULL, (char *)exe.c_str(), NULL, NULL, TRUE, 0, NULL, NULL, &si, &pi))
 	{
 		CloseHandle(pi.hThread);
-		const int max = 500;
+		const int max = 20;
 		char buf[max] = { 0 };
 		DWORD dw;
-
-		if (ReadFile(hReadPipe, buf, max - 1, &dw, NULL))
-		{
-			cout << buf << endl;
-			//			ZeroMemory(buf,max);
-		}
-
+		ReadFile(hOutReadPipe, buf, max - 1, &dw, NULL);
+		cout << buf << endl;
 		CloseHandle(pi.hProcess);
 	}
 
-	CloseHandle(hReadPipe);
-	CloseHandle(hWritePipe);
+	CloseHandle(hInReadPipe);
+	CloseHandle(hInWritePipe);
+	CloseHandle(hOutWritePipe);
+	CloseHandle(hOutReadPipe);
 }
---
+++ MysqlPipe/hello/main.cpp
@@ -0,0 +1,22 @@
+//#include <iostream>
+
+//using namespace std;
+
+/*void main() {
+	cout << "test" << endl;
+	int data;
+	while (cin >> data) {
+		cout << data << endl;
+	}
+}*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	//fprintf(stderr, "c");
+	//cout << "c"<<endl;
+	printf("c");
+	getchar();
+}
